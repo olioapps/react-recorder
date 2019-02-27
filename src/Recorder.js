d2c30378538f26244002551b7022242200bfcfa6
rachoac <aronracho@yahoo.com>
2019-02-26 18:52:01 -0800

    Update Recorder.js

--- src/Recorder.js
+++ src/Recorder.js
@@ -1,99 +1,100 @@
-'use strict'
+import React from 'react'
+import PropTypes from 'prop-types';
 
-import React, { PropTypes } from 'react'
+class Recorder extends React.Component {
 
-const Recorder = React.createClass({
-  start () {
-    this.mediaRecorder.start()
-  },
+	start () {
+		this.mediaRecorder.start()
+	}
 
-  stop () {
-    this.mediaRecorder.stop()
-  },
+	stop () {
+		this.mediaRecorder.stop()
+	}
 
-  pause () {
-    this.mediaRecorder.pause()
-  },
+	pause () {
+		this.mediaRecorder.pause()
+	}
 
-  resume () {
-    this.mediaRecorder.resume()
-  },
+	resume () {
+		this.mediaRecorder.resume()
+	}
 
-  componentDidMount () {
-    navigator.getUserMedia = (navigator.getUserMedia ||
-                              navigator.mozGetUserMedia ||
-                              navigator.msGetUserMedia ||
-                              navigator.webkitGetUserMedia)
+	componentDidMount () {
+		navigator.getUserMedia = (navigator.getUserMedia ||
+			navigator.mozGetUserMedia ||
+			navigator.msGetUserMedia ||
+			navigator.webkitGetUserMedia);
 
-    if (navigator.getUserMedia && window.MediaRecorder) {
-      const constraints = {audio: true}
-      this.chunks = []
-      const { blobOpts, onStop, onError, mediaOpts, onPause, onResume, onStart, gotStream } = this.props
+		if (navigator.getUserMedia && window.MediaRecorder) {
+			const constraints = {audio: true};
+			this.chunks = [];
+			const { blobOpts, onStop, onError, mediaOpts, onPause, onResume, onStart, gotStream } = this.props;
 
-      const onErr = err => {
-        console.warn(err)
-        if (onError) onError(err)
-      }
+			const onErr = err => {
+				console.warn(err);
+				if (onError) onError(err)
+			};
 
-      const onSuccess = stream => {
-        this.mediaRecorder = new window.MediaRecorder(stream, mediaOpts || {})
+			const onSuccess = stream => {
+				this.mediaRecorder = new window.MediaRecorder(stream, mediaOpts || {});
 
-        this.mediaRecorder.ondataavailable = e => {
-          this.chunks.push(e.data)
-        }
+				this.mediaRecorder.ondataavailable = e => {
+					this.chunks.push(e.data)
+				};
 
-        this.mediaRecorder.onstop = e => {
-          const blob = new window.Blob(this.chunks, blobOpts || {type: 'audio/wav'})
-          this.chunks = []
-          onStop(blob)
-        }
+				this.mediaRecorder.onstop = e => {
+					const blob = new window.Blob(this.chunks, blobOpts || {type: 'audio/wav'});
+					this.chunks = [];
+					onStop(blob)
+				};
 
-        this.mediaRecorder.onerror = onErr
-        if (onPause) this.mediaRecorder.onpause = onPause
-        if (onResume) this.mediaRecorder.onresume = onResume
-        if (onStart) this.mediaRecorder.onstart = onStart
-        this.stream = stream
-        if (gotStream) gotStream(stream)
-      }
+				this.mediaRecorder.onerror = onErr;
+				if (onPause) this.mediaRecorder.onpause = onPause;
+				if (onResume) this.mediaRecorder.onresume = onResume;
+				if (onStart) this.mediaRecorder.onstart = onStart;
+				this.stream = stream;
+				if (gotStream) gotStream(stream)
+			};
 
-      navigator.getUserMedia(constraints, onSuccess, onErr)
-    } else {
-      console.warn('Audio recording APIs not supported by this browser')
-      const { onMissingAPIs } = this.props
-      if (onMissingAPIs) {
-        onMissingAPIs(navigator.getUserMedia, window.MediaRecorder)
-      } else {
-        window.alert('Your browser doesn\'t support native microphone recording. For best results, we recommend using Google Chrome or Mozilla Firefox to use this site.')
-      }
-    }
-  },
+			navigator.getUserMedia(constraints, onSuccess, onErr)
+		} else {
+			console.warn('Audio recording APIs not supported by this browser');
+			const { onMissingAPIs } = this.props;
+			if (onMissingAPIs) {
+				onMissingAPIs(navigator.getUserMedia, window.MediaRecorder)
+			} else {
+				window.alert('Your browser doesn\'t support native microphone recording. For best results, we recommend using Google Chrome or Mozilla Firefox to use this site.')
+			}
+		}
+	}
 
-  componentDidUpdate (prevProps) {
-    if (this.props.command && this.props.command !== 'none' && prevProps.command !== this.props.command) {
-      this[this.props.command]()
-    }
-  },
+	componentDidUpdate (prevProps) {
+		if (this.props.command && this.props.command !== 'none' && prevProps.command !== this.props.command) {
+			this[this.props.command]()
+		}
+	}
 
-  componentWillUnmount () {
-    if (this.props.onUnmount) this.props.onUnmount(this.stream)
-  },
+	componentWillUnmount () {
+		if (this.props.onUnmount) this.props.onUnmount(this.stream)
+	}
 
-  render () {
-    return false
-  },
+	render () {
+		return false
+	}
+}
 
-  propTypes: {
-    onStop: PropTypes.func.isRequired,
-    onMissingAPIs: PropTypes.func,
-    onError: PropTypes.func,
-    onPause: PropTypes.func,
-    onStart: PropTypes.func,
-    onResume: PropTypes.func,
-    onUnmount: PropTypes.func,
-    gotStream: PropTypes.func,
-    blobOpts: PropTypes.object,
-    mediaOpts: PropTypes.object
-  }
-})
+Recorder.propTypes = {
+	command: PropTypes.oneOf(['start', 'stop', 'pause', 'resume', 'none']),
+	onStop: PropTypes.func.isRequired,
+	onMissingAPIs: PropTypes.func,
+	onError: PropTypes.func,
+	onPause: PropTypes.func,
+	onStart: PropTypes.func,
+	onResume: PropTypes.func,
+	onUnmount: PropTypes.func,
+	gotStream: PropTypes.func,
+	blobOpts: PropTypes.object,
+	mediaOpts: PropTypes.object
+};
 
 export default Recorder
